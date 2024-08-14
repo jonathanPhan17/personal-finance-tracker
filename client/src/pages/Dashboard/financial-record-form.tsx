217e343573e21ba44d2e189ad0aafbc854bc1946
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-14 15:33:00 -0700

    add form functionality

--- client/src/pages/Dashboard/financial-record-form.tsx
+++ client/src/pages/Dashboard/financial-record-form.tsx
@@ -1,6 +1,69 @@
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+
 export const FinancialRecordForm = () => {
 
+    const [description, setDescription] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [paymentMethod, setPaymentMethod] = useState<string>("");
+
+    const { user } = useUser();
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const newRecord = {
+            userId: user?.id,
+            date: new Date(),
+            description: description,
+            amount: parseFloat(amount),
+            category: category,
+            paymentMethod: paymentMethod,
+        };
+
+        setDescription("")
+        setAmount("")
+        setCategory("")
+        setPaymentMethod("")
+    }
+
     return (
-         <div>Record Form</div>
+         <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <div className="form-field">
+                    <label htmlFor="description">Description:</label>
+                    <input type="text" name="description" className="input" value=  {description} onChange={(e) => setDescription(e.target.value)}      required />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="amount">Amount:</label>
+                    <input type="number" name="amount" className="input" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="category">Category</label>
+                    <select name="category" className="input" value={category} onChange={(e) => setCategory(e.target.value)} required>
+                        <option value="">Select a Category</option>
+                        <option value="Food">Food</option>
+                        <option value="Rent">Rent</option>
+                        <option value="Salary">Salary</option>
+                        <option value="Utilities">Utilities</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
+                <div className="form-field">
+                    <label htmlFor="payment">Payment</label>
+                    <select name="payment" className="input" value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)} required>
+                        <option value="">Select a Payment Method</option>
+                        <option value="Credit Card">Credit Card</option>
+                        <option value="Cash">Cash</option>
+                        <option value="Bank Transfer">Bank Transfer</option>
+                    </select>
+                </div>
+                <button type="submit" className="button">
+                    Add Record
+                </button>
+            </form>
+         </div>
     )
 }
