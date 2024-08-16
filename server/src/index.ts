5813a15aad4602bd6a089db76d6ba8337330322a
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-16 12:42:07 -0700

    Initialize Node.js with Express, Mongoose, and fix MongoDB connection

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string = process.env.MONGO_URI || '';
+
+mongoose.connect(mongoURI)
+.then(() => console.log(`CONNECTED TO MONGODB!`))
+.catch((err) => console.error("failed to connect to MongoDB:", err));
+
+app.listen(port, () => {
+    console.log(`Server running on PORT ${port}`);
+})
