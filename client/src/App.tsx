0e1974d41cdf0b5468fcb429f0a731ee66cadc2a
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-17 16:00:58 -0700

    add context for managing financial records with fetch and add functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 // Pages
 import { Dashboard } from './pages/Dashboard';
 import { Auth } from './pages/Auth';
+import { FinancialRecordsProvider } from './contexts/financial-record-context';
 
 function App() {
 
@@ -11,7 +12,14 @@ function App() {
     <Router>
       <div className='app-container'>
         <Routes>
-          <Route path='/' element={<Dashboard />} />
+          <Route 
+            path='/' 
+            element={
+            <FinancialRecordsProvider>
+              <Dashboard />
+            </FinancialRecordsProvider>
+          } 
+          />
           <Route path='/auth' element={<Auth />} />
         </Routes>
       </div>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import financialRecordRouter from './routes/financial-records';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,12 +10,15 @@ const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 const mongoURI: string = process.env.MONGO_URI || '';
 
 mongoose.connect(mongoURI)
 .then(() => console.log(`CONNECTED TO MONGODB!`))
 .catch((err) => console.error("failed to connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
     console.log(`Server running on PORT ${port}`);
