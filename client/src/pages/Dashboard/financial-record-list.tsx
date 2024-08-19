c7bd6dcd0c26269ad727cdc9b66e7469fb48e16b
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-18 19:37:24 -0700

    add delete

--- client/src/pages/Dashboard/financial-record-list.tsx
+++ client/src/pages/Dashboard/financial-record-list.tsx
@@ -44,52 +44,82 @@ const EditableCell: React.FC<EditableCellProps> = (
 
 export const FinancialRecordList= () => {
 
-    const { records } = useFinancialRecords();
+    const { records, updateRecord, deleteRecord } = useFinancialRecords();
 
-    const columns: Array<Column<FinancialRecord>> = useMemo(() => [
+    const updateCellRecord = (rowIndex: number, columnId: string, value: any) => {
+        const id = records[rowIndex]?._id;
+        updateRecord(id ?? "", {...records[rowIndex], [columnId]: value });
+    }
+
+    const columns: Array<Column<FinancialRecord>> = useMemo(
+      () => [
         {
-            Header: "Description",
-            accessor: "description",
-            Cell: (props: any) => (
-                <EditableCell {...props} updateRecord={() => null} editable={true} />
-            ),
+          Header: "Description",
+          accessor: "description",
+          Cell: (props) => (
+            <EditableCell
+              {...props}
+              updateRecord={updateCellRecord}
+              editable={true}
+            />
+          ),
         },
         {
-            Header: "Amount",
-            accessor: "amount",
-            Cell: (props: any) => (
-                <EditableCell {...props} updateRecord={() => null} editable={true} />
-            ),
+          Header: "Amount",
+          accessor: "amount",
+          Cell: (props) => (
+            <EditableCell
+              {...props}
+              updateRecord={updateCellRecord}
+              editable={true}
+            />
+          ),
         },
         {
-            Header: "Category",
-            accessor: "category",
-            Cell: (props: any) => (
-                <EditableCell {...props} updateRecord={() => null} editable={true} />
-            ),
+          Header: "Category",
+          accessor: "category",
+          Cell: (props) => (
+            <EditableCell
+              {...props}
+              updateRecord={updateCellRecord}
+              editable={true}
+            />
+          ),
         },
         {
-            Header: "Payment Method",
-            accessor: "paymentMethod",
-            Cell: (props: any) => (
-                <EditableCell {...props} updateRecord={() => null} editable={true} />
-            ),
+          Header: "Payment Method",
+          accessor: "paymentMethod",
+          Cell: (props: any) => (
+            <EditableCell
+              {...props}
+              updateRecord={updateCellRecord}
+              editable={true}
+            />
+          ),
         },
         {
-            Header: "Date",
-            accessor: "date",
-            Cell: (props: any) => (
-                <EditableCell {...props} updateRecord={() => null} editable={false} />
-            ),
+          Header: "Date",
+          accessor: "date",
+          Cell: (props: any) => (
+            <EditableCell
+              {...props}
+              updateRecord={updateCellRecord}
+              editable={false}
+            />
+          ),
         },
         {
-            Header: "Delete",
-            id: "delete",
-            Cell: ({ row }) => (
-                <button onClick={() => null} className="button">Delete</button>
-            ),
+          Header: "Delete",
+          id: "delete",
+          Cell: ({ row }) => (
+            <button onClick={() => deleteRecord(row.original._id ?? "")} className="button">
+              Delete
+            </button>
+          ),
         },
-    ], [])
+      ],
+      [records]
+    );
 
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({columns, data: records })
 
